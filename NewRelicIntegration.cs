aef80d72cc2d2997709edfbb7da818dde86d15f8
Bedriye Koç <bedriye.koc@gozen.systems>
2023-05-24 21:49:37 +0300

    test commit

---
+++ NewRelicIntegration.cs
@@ -0,0 +1,30 @@
+﻿using Amazon.DynamoDBv2.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PinPointCallerVersionOne
+{
+    /// <summary>
+    /// Dynamo db table declaration
+    /// </summary>
+
+    [DynamoDBTable("new_relic_integration")]
+    public class NewRelicIntegration
+    {
+        [DynamoDBHashKey("id")]
+        public int Id { get; set; }
+
+        public string policy_key { get; set; }
+
+        public string employee { get; set; }
+
+        public string phone_number { get; set; }
+
+        public bool is_deleted { get; set; }
+
+        public int order { get; set; }
+    }
+}
---
+++ PinPointHandler.cs
@@ -0,0 +1,62 @@
+﻿using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+using Amazon.DynamoDBv2.DocumentModel;
+using Amazon.Runtime;
+
+namespace PinPointCallerVersionOne
+{
+    public class PinPointHandler : IPinPointHandler
+    {
+        private readonly ILogger<PinPointHandler> Logger;
+        private readonly IDynomaDBHandler _dynomaDBHandler;
+        private readonly IPinPointServiceHandler _pinPointServiceHandler;
+        public PinPointHandler(ILogger<PinPointHandler> logger,
+            IDynomaDBHandler dynomaDBHandler,
+            IPinPointServiceHandler pinPointServiceHandler)
+        {
+            Logger = logger;
+            _dynomaDBHandler = dynomaDBHandler;
+            _pinPointServiceHandler = pinPointServiceHandler;
+        }
+        public void Handler(EventData eventData)
+        {
+            try
+            {
+                if (eventData == null)
+                    throw new NullReferenceException(nameof(eventData));
+                var employees = _dynomaDBHandler.Get(string.Join(",", eventData.AlertPolicyNames));
+                if (employees == null)
+                    Logger.LogInformation("Responsible  employess not found");
+                SendVoiceCall(eventData, employees);
+
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex.Message, ex);
+
+            }
+
+        }
+
+        private void SendVoiceCall(EventData eventData, IEnumerable<NewRelicIntegration>? employees)
+        {
+            foreach (var item in employees)
+            {
+                try
+                {
+                    _pinPointServiceHandler.SendVoiceMessage(item.employee, item.phone_number, eventData);
+                }
+                catch(Exception ex)
+                {
+                    Logger.LogError(ex.Message, ex);
+                }
+            }
+        }
+    }
+    public interface IPinPointHandler
+    {
+        void Handler(EventData eventData);
+    }
+
+
+}
